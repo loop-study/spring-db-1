bee97f679d3fac6e57caed3e743f4108f80da163
apdkeb <apdkeb@naver.com>
2024-06-09 16:22:01 +0900

    데이터 접근 기술 - JPA jpa 설정 추가

--- itemservice-db/src/main/java/hello/itemservice/config/MyBatisConfig.java
+++ itemservice-db/src/main/java/hello/itemservice/config/MyBatisConfig.java
@@ -9,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 @RequiredArgsConstructor
 public class MyBatisConfig {
